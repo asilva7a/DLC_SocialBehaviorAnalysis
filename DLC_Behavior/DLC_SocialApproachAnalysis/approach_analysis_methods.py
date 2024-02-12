77e85138ceb7b2981c2b5a9e7e9082f5f8b59d43
asilva7a <silva7a@cmich.edu>
2024-02-12 13:30:14 -0500

    I wish autosave was a thing

--- DLC_Behavior/DLC_SocialApproachAnalysis/approach_analysis_methods.py
+++ DLC_Behavior/DLC_SocialApproachAnalysis/approach_analysis_methods.py
@@ -27,22 +27,6 @@ class ApproachAnalysis_Organizer:
                 all_files.append(os.path.join(root, file))
         return all_files
     
-    def initialize_directory_df(self):
-        directories = [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))]
-        directory_data = [{'directory_name': d, 'directory_path': os.path.join(self.project_folder, d)} for d in directories]
-        return pd.DataFrame(directory_data, columns=['directory_name', 'directory_path'])
-    
-    def list_directories(self):
-        return [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))]
-    
-    def list_files(self, folder_name):
-        folder_path = os.path.join(self.project_folder, folder_name)
-        all_files = []
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                all_files.append(os.path.join(root, file))
-        return all_files
-    
     # Method to get user input for animal IDs and treatments
     def get_user_input(self):
         self.animal_data = {} # Initialize an empty dictionary to store animal IDs and treatments
