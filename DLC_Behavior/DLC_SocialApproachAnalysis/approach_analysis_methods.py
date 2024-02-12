a0da777c803d494f328bc6fd35c7f7bb1d9c48ff
asilva7a <silva7a@cmich.edu>
2024-02-12 16:46:04 -0500

    Not sure why the commit isn't sticking

--- DLC_Behavior/DLC_SocialApproachAnalysis/approach_analysis_methods.py
+++ DLC_Behavior/DLC_SocialApproachAnalysis/approach_analysis_methods.py
@@ -44,9 +44,26 @@ class UserInput(ApproachAnalysis_Organizer):
                 print(f'Animal ID {animal_id} already exists. Please enter a different animal ID.')
             else:
                 treatments = self.get_treatments(animal_id)
-                self.animal_data[animal_id] = treatments  
-                    
+                self.animal_data[animal_id] = treatments             
 
+    # Method to get treatments for a given animal ID
+    def get_treatments(self, animal_id):
+        while True:
+            treatments = input(f'Enter the three treatments for animal ID {animal_id}, separated by commas: ').split(',') 
+            if len(treatments) == 3:
+                return treatments 
+            else:
+                print('Invalid number of treatments. Please enter exactly three treatments.') 
+
+    # Method to organize files based on animal IDs and treatments  
+    def organize_files(self):
+        for animal_id, treatments in self.animal_data.items():  # Iterate over each animal ID and its treatments
+            for treatment in treatments:
+                treatment_folder = os.path.join(self.project_folder, treatment.strip())  # Create a path for the treatment folder
+                if not os.path.exists(treatment_folder):
+                    os.makedirs(treatment_folder)  
+                animal_folder = os.path.join(treatment_folder, animal_id.strip())  # Create a path for the animal folder within the treatment folder
+                if not os.path.exists(animal_folder):
                     os.makedirs(animal_folder) 
                 for file in self.directory_df[self.directory_df['directory'].str.contains(animal_id)].values:
                     file_path = os.path.join(file[1], file[0])  # Get the full path of the file
